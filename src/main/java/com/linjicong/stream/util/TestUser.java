f7b2c8e252324a01346003ff50d36b2752f28f26
linjicong <932997844@qq.com>
2022-04-14 09:15:54 +0800

    Initial commit

---
+++ src/main/java/com/linjicong/stream/util/TestUser.java
@@ -0,0 +1,18 @@
+package com.linjicong.stream.util;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+/**
+ * @author linjicong
+ * @date 2022-04-13 9:26
+ */
+@Data
+@AllArgsConstructor
+public class TestUser {
+    private String name;
+    private Long deptId;
+    private Double score;
+    private Integer winningCount;
+    private Long number;
+}
